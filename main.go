40d360c50e2d99f0e1dfe1e185ad95e8687aaaea
assilbekov <askhat.assilbekov@gmail.com>
2024-05-02 15:56:00 -0300

    finish

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"proxy/pkg"
+)
+
+var (
+	admin = "admin"
+	user  = "user"
+	users = map[string]bool{
+		admin: true,
+		user:  false,
+	}
+)
+
 func main() {
-	//
+	db := &pkg.Database{}
+	proxy := &pkg.ProxyDatabase{
+		Users: users,
+		DB:    db,
+	}
+
+	data, err := proxy.GetData(admin)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(data)
+
+	data, err = proxy.GetData(user)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
--- pkg/Database.go
+++ pkg/Database.go
@@ -5,5 +5,5 @@ type Database struct {
 }
 
 func (d *Database) GetData(user string) ([]string, error) {
-	return []string{"data1", "data2"}, nil
+	return []string{"text 1", "data2"}, nil
 }
